18ab52273fa535f7621cb410a4be0ae75c8d9ca4
Shreyansh Pandey <shreyansh.fractal@gmail.com>
2024-01-17 02:27:47 +0530

    ,

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,13 +1,11 @@
 import Connect from "@/database/mongo.config";
 import  Post  from "@/models/posts";
 import { NextResponse } from "next/server";
-import { revalidatePath } from "next/cache";
 
 
-export async function GET(request){
+export const revalidate = 1;
+export async function GET(){
     await Connect();
     const res = await Post.find().populate('author').sort({createdAt:-1}).limit(20).exec();
-    const path = request.nextUrl.searchParams.get("path") || "/";
-    revalidatePath(path);
     return NextResponse.json(res);
 }
