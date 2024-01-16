59f61f2ddea81f57d1a90f25382d30f0d76f6453
Shreyansh Pandey <shreyansh.fractal@gmail.com>
2024-01-17 01:23:32 +0530

    ..

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -6,6 +6,6 @@ import { NextResponse } from "next/server";
 
 export async function GET(){
     await Connect();
-    const res = await Post.find({author:null}).populate('author').sort({createdAt:-1}).limit(20);
+    const res = await Post.find().populate('author').sort({createdAt:-1}).limit(20);
     return NextResponse.json(res);
 }
