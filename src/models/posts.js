36d3088d2ee5a1245bcc149135d942b4efb45f50
Shreyansh Pandey <shreyansh.fractal@gmail.com>
2024-01-17 11:52:27 +0530

    ...

--- src/models/posts.js
+++ src/models/posts.js
@@ -2,7 +2,7 @@ const { Schema, default: mongoose, models, model } = require("mongoose");
 
 const Postschema = new Schema({
     author: { type: mongoose.Types.ObjectId, ref: 'User', required: 'true' },
-    text: { type: String, minlength: 5, maxlength: 255, required: true },
+    text: { type: String, minlength: 5, maxlength: 2555, required: true },
     likeCount: { type: Number, required: true,default:0 },
     replyCount: { type: Number, required: true,default:0 },
     parent: { type: mongoose.Types.ObjectId, ref: 'Post' }
